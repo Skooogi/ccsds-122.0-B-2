RLE error handling and sign words now appear in the correct positions in the bitstream

python/bpe.py
@@ -188,9 +188,14 @@ def encode(data, width, height, pad_width):
     num = 0
     sym_avg = 0
     total = len(blocks)*(bitACGlobal-1)*4
-    for b in range(bitACGlobal-1, -1, -1):
 
-        for stage in range(1):
+    twobit = 0
+    threebit = 0
+    fourbit = 0
+
+    for b in range(bitACGlobal-1, bitACGlobal-4, -1):
+
+        for stage in range(2):
 
             for gaggle in range(0, len(blocks), 16):
 
@@ -214,40 +219,55 @@ def encode(data, width, height, pad_width):
                 bit3 = np.argmin(bitstream.code.options[1])
                 bit4 = np.argmin(bitstream.code.options[2]) 
 
+                first = 0
+
                 for idx in range(len(bitstream.code.words)):
                     word = bitstream.code.words[idx]
                     size = bitstream.code.sizes[idx]
                     sym = bitstream.code.symbol_option[idx]
 
+                    if(size < 0):
+                        bitstring += format(word, f'0{size*-1}b')
+                        continue
                     if(size == 1):
-                        bitstring += str(int(word))
+                        bitstring += format(word, '01b')
                         continue
                     if(size == 2):
+                        if(not first & 1):
+                            bitstring += format(bit2,'01b')
+                            print("2",format(bit2, '01b'))
+                            first |= 1
                         bitstring += bitstream.code.word2bit[bit2][bitstream.code.sym2bit[int(word)]]
                         continue
                     if(size == 3):
+                        if(not first & 2):
+                            print("3",format(bit3, '02b'))
+                            bitstring += format(bit3,'02b')
+                            first |= 2
                         bitstring += bitstream.code.word3bit[bit3][bitstream.code.sym3bit[sym][int(word)]]
                         continue
                     if(size == 4):
+                        if(not first & 4):
+                            print("4",format(bit4, '02b'))
+                            bitstring += format(bit4,'02b')
+                            first |= 4
                         bitstring += bitstream.code.word4bit[bit4][bitstream.code.sym4bit[sym][int(word)]]
                         continue
 
                 if(sum(bitstream.code.sizes) != 0):
-                    #bitstream.out_bits(bitstring)
+                    print(gaggle, bitstring)
+                    bitstream.out_bits(bitstring)
                     temp = len(bitstring)/sum(bitstream.code.sizes)
                     sym_avg += temp
                     num += 1
                     #if(gaggle/16 % 32 == 0):
                     #    print(b,"Gaggle:", int(gaggle/16),", sym:", temp)
         #code.stage_4(blocks, b)
-
         progress = len(blocks)*4*(bitACGlobal-1-b)/total*100
-        print(f'Encoded {progress:3.1f}%',end='\r')
+        #print(f'Encoded {progress:3.1f}%',end='\r')
 
-    print()
-
-    if(bitstream.out.size != 0):
-        bitstream.out(0, 8-bitstream.out.size)
+    #if(bitstream.out.size != 0):
+        #bitstream.out(0, 8)
 
     if(num > 0):
         print("AVG:",sym_avg/num)

python/code.py
@@ -99,7 +99,6 @@ def encode_dc_magnitudes(blocks, bitDC, q):
     #First DC coefficient is uncoded
     diffs = np.zeros(len(blocks), dtype='int')
     for i in range(len(blocks)):
-        print(blocks[i].dc)
         blocks[i].dc = twos(blocks[i].dc, bitDC)
     
     last = int(blocks[0].dc / pow(2, q))
@@ -234,7 +233,9 @@ def stage_1(blocks, b):
         size_s = 0
         size_p = 0
 
-        if(0 <= status_to_int(blocks[i], 0) == 1):
+        #print(status_to_int(blocks[i], 0),status_to_int(blocks[i], 21),status_to_int(blocks[i], 42))
+
+        if(0 <= status_to_int(blocks[i], 0) <= 1):
             types_p |= (abs(blocks[i].ac[0]) >> b) & 1
             size_p += 1
 
@@ -266,7 +267,7 @@ def stage_1(blocks, b):
             #print(b,i,"P",bin(types_p)[2:], size_p, bin(signs_p)[2:], size_s)
             bitstream.code(types_p, size_p, 0)
             if(size_s > 0):
-                bitstream.out(signs_p, size_s)
+                bitstream.code(signs_p, size_s, 0, -1)
   
 
 def stage_2(blocks, b):

python/rle.py
@@ -9,6 +9,12 @@ class NodeTree(object):
         self.left = left
         self.right = right
 
+    def has_left(self):
+        return not self.left == None
+
+    def has_right(self):
+        return not self.right == None
+
     def children(self):
         return self.left, self.right
 
@@ -31,14 +37,19 @@ def read_tree(node, binstr):
 
     bits_traversed = 0
     curr_node = node
-    while type(curr_node) != np.int64 and len(binstr) != 0:
-        if(binstr[0] == '0'):
+    while(type(curr_node) == NodeTree) and len(binstr) != 0:
+        if(binstr[0] == '0' and curr_node.has_left()):
             curr_node = curr_node.left
-        else:
+        elif(binstr[0] == '1' and curr_node.has_right()):
             curr_node = curr_node.right
+        else:
+            return curr_node, -1
 
         bits_traversed += 1
         binstr = binstr[1:]
+
+    if(type(curr_node) == NodeTree):
+        return None, -1
 
     return curr_node, bits_traversed
 
@@ -173,6 +184,7 @@ def uncompress(filein, fileout):
                 value, skip = read_tree(huffman_tree, binstr)
 
 if __name__ == '__main__':
+    
     args = sys.argv
     if(len(args) > 3 and args[1] == "compress"):
         compress(args[2], args[3])
@@ -181,5 +193,3 @@ if __name__ == '__main__':
 
     else:
          print("Input error")
-    
-