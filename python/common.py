f41034e76a975f136324a82bc9c336e8d1275e72
Skooogi <kassuskog@gmail.com>
2023-06-14 18:30:37 +0300

    Enhanced block printing

--- python/common.py
+++ python/common.py
@@ -25,54 +25,90 @@ class Block(object):
         self.bmax       = 0 #1 bit
         self.dmax       = np.ones(3, dtype=int)
 
+    def get_bmax(self):
+        temp = -1
+        temp = max(temp, self.get_dmax(0))
+        temp = max(temp, self.get_dmax(1))
+        temp = max(temp, self.get_dmax(2))
+        return temp
+
+    def get_dmax(self, family):
+        temp = -1
+        for i in range(20):
+            if self.get_status(21*family+1+i) == 0:
+                temp = 0
+            elif self.get_status(21*family+1+i) == 1:
+                return 1
+        return temp
+
+    def get_status(self, i):
+        temp = (((self.status1 >> i) & 1) << 1) | ((self.status2 >> i) & 1)
+        return temp if temp < 3 else -1
+
+    def set_status(self, ac_index, value):
+        temp = 3 if value == -1 else value
+
+        reset = ~(1 << ac_index)
+        self.status1 &= reset
+        self.status2 &= reset
+
+        self.status1 |= ((temp >> 1) & 1) << ac_index
+        self.status2 |= (temp & 1) << ac_index
+
     def __str__(self):
-        #TODO: print in correct raster order
         output = 'BLOCK:\nDC\t' + format(int(self.dc),'04d')+'\n'
         output += f'bitDC\t{self.bitAC}\nbitAC\t{self.bitAC}\n'
         for family in range(3):
             output += 'Family '+str(family) + '\n'
-            output += '\tp\t' + format(self.ac[family*21+0], '04d') + '\n'
+            output += '\tp\t' + format(self.ac[family*21+0], '04d')
+            output += '\t\t    |' + format(self.get_status(family*21+0), '02d') + '\n'
             output += '\tc\t' + format(self.ac[family*21+1], '04d')
             output += ' ' + format(self.ac[family*21+2], '04d')
             output += ' ' + format(self.ac[family*21+3], '04d')
-            output += ' ' + format(self.ac[family*21+4], '04d') + '\n'
+            output += ' ' + format(self.ac[family*21+4], '04d')
+            output += ' |' + format(self.get_status(family*21+1), '02d')
+            output += ' ' + format(self.get_status(family*21+2), '02d')
+            output += ' ' + format(self.get_status(family*21+3), '02d')
+            output += ' ' + format(self.get_status(family*21+4), '02d') + '\n'
 
             output += f'\tH{family}0\t' + format(self.ac[family*21+5], '04d')
             output += ' ' + format(self.ac[family*21+6], '04d')
             output += ' ' + format(self.ac[family*21+7], '04d')
-            output += ' ' + format(self.ac[family*21+8], '04d') + '\n'
+            output += ' ' + format(self.ac[family*21+8], '04d')
+            output += ' |' + format(self.get_status(family*21+5), '02d')
+            output += ' ' + format(self.get_status(family*21+6), '02d')
+            output += ' ' + format(self.get_status(family*21+7), '02d')
+            output += ' ' + format(self.get_status(family*21+8), '02d') + '\n'
 
             output += f'\tH{family}1\t' + format(self.ac[family*21+9], '04d')
             output += ' ' + format(self.ac[family*21+10], '04d')
             output += ' ' + format(self.ac[family*21+11], '04d')
-            output += ' ' + format(self.ac[family*21+12], '04d') + '\n'
+            output += ' ' + format(self.ac[family*21+12], '04d')
+            output += ' |' + format(self.get_status(family*21+9), '02d')
+            output += ' ' + format(self.get_status(family*21+10), '02d')
+            output += ' ' + format(self.get_status(family*21+11), '02d')
+            output += ' ' + format(self.get_status(family*21+12), '02d') + '\n'
 
             output += f'\tH{family}2\t' + format(self.ac[family*21+13], '04d')
             output += ' ' + format(self.ac[family*21+14], '04d')
             output += ' ' + format(self.ac[family*21+15], '04d')
-            output += ' ' + format(self.ac[family*21+16], '04d') + '\n'
+            output += ' ' + format(self.ac[family*21+16], '04d')
+            output += ' |' + format(self.get_status(family*21+13), '02d')
+            output += ' ' + format(self.get_status(family*21+14), '02d')
+            output += ' ' + format(self.get_status(family*21+15), '02d')
+            output += ' ' + format(self.get_status(family*21+16), '02d') + '\n'
 
             output += f'\tH{family}3\t' + format(self.ac[family*21+17], '04d')
             output += ' ' + format(self.ac[family*21+18], '04d')
             output += ' ' + format(self.ac[family*21+19], '04d')
-            output += ' ' + format(self.ac[family*21+20], '04d') + '\n'
+            output += ' ' + format(self.ac[family*21+20], '04d')
+            output += ' |' + format(self.get_status(family*21+17), '02d')
+            output += ' ' + format(self.get_status(family*21+18), '02d')
+            output += ' ' + format(self.get_status(family*21+19), '02d')
+            output += ' ' + format(self.get_status(family*21+20), '02d') + '\n'
 
         return output
 
-
-def status_to_int(block, i):
-    temp = (((block.status1 >> i) & 1) << 1) | ((block.status2 >> i) & 1)
-    return temp if temp < 3 else -1
-
-def int_to_status(block, ac_index, temp):
-    val = 3 if temp == -1 else temp
-
-    reset = ~(1 << ac_index)
-    block.status1 &= reset
-    block.status2 &= reset
-
-    block.status1 |= ((val >> 1) & 1) << ac_index
-    block.status2 |= (val & 1) << ac_index
 
 
 def subband_lim(ac_index, current_bitplane):
